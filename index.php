e993ea62169a8b50edd2f8c3c356f10b7f17deb3
Bilal Kuzey <bk1405bgl@gmail.com>
2023-07-26 15:27:34 +0200

    basic style system aufgesetzt

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-    echo "Hello World!";
-?>
+<DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="PHP Projekte">
+    <meta name="author" content="Bilal Kuzey">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="PHP, PHP-Projekte">
+    <title>PHP Projekte</title>
+    <link rel="stylesheet" type="text/css" href="reset.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>PHP Test</h1>
+    <p>This is HTML</p>
+    <?php
+        echo "This is PHP!";
+    ?>
+</body>
+</html>
