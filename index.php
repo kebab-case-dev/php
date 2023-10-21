ec6b26fff2435cb15ec8bac363b5fad6089859b6
Bilal Kuzey <bk1405bgl@gmail.com>
2023-10-21 16:18:30 +0200

    Sauber eingerückt

--- index.php
+++ index.php
@@ -11,18 +11,18 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<div class="container">
-    <h1 class="title">PHP Projekte</h1>
-    <div class="content">
-        <?php
-            $projekte = scandir('.');
-            foreach ($projekte as $projekt) {
-                if (is_dir($projekt) && $projekt != '.' && $projekt != '..') {
-                    echo '<div class="card"><a href="' . $projekt . '">' . $projekt . '</a></div>';
+    <div class="container">
+        <h1 class="title">PHP Projekte</h1>
+        <div class="content">
+            <?php
+                $projekte = scandir('.');
+                foreach ($projekte as $projekt) {
+                    if (is_dir($projekt) && $projekt != '.' && $projekt != '..') {
+                        echo '<div class="card"><a href="' . $projekt . '">' . $projekt . '</a></div>';
+                    }
                 }
-            }
-        ?>
+            ?>
+        </div>
     </div>
-</div>
 </body>
 </html>
